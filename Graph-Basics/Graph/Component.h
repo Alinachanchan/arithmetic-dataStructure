d863d793f7e0847e6b6ded4e19297cf6d748a591
wangchan <1041436848@qq.com>
2017-03-13 19:55:17 +0800

    add Shortest-Path Minimum-Span-Trees

---
+++ Graph-Basics/Graph/Component.h
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+
+template <typename Graph>
+class Component{
+
+private:
+    Graph &G;
+    bool* visited;
+    int ccount = 0;
+    int *id;
+
+    void dfs( int v ){
+        visited[v] = true;
+        id[v] = ccount;
+        typename Graph::adjIterator adj(G,v);
+        for( int i = adj.begin() ; !adj.end() ; i = adj.next() )
+            if( !visited[i] )
+                dfs(i);
+
+    }
+
+public:
+    Component(Graph &graph):G(graph){
+
+        visited = new bool[G.V()];
+        id = new int[G.V()];
+        for( int i = 0 ; i < G.V() ; i ++ ){
+            visited[i] = false;
+            id[i] = -1;
+        }
+        ccount = 0;
+
+        for( int i = 0 ; i < G.V() ; i ++ )
+            if( !visited[i] ){
+                dfs(i);
+                ccount += 1;
+            }
+    }
+
+    ~Component(){
+
+        delete [] visited;
+        delete [] id;
+    }
+
+    int count(){
+        return ccount;
+    }
+
+    bool isConnected( int v , int w ){
+
+        assert( v >= 0 && v < G.V() );
+        assert( w >= 0 && w < G.V() );
+        assert( id[v] != -1 && id[w] != -1 );
+        return id[v] == id[w];
+    }
+};
---
+++ Minimum-Span-Trees/MinimumSpanTree/Edge.h
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+
+template<typename Weight>
+class Edge{
+private:
+    int a, b;
+    Weight weight;
+
+public:
+    Edge(int a, int b, Weight weight){
+        this->a = a;
+        this->b = b;
+        this->weight = weight;
+    }
+
+    Edge(){}
+
+    ~Edge(){}
+
+    int v(){ return a;}
+
+    int w(){ return b;}
+
+    Weight wt(){ return weight;}
+
+    int other(int x){
+        assert( x == a || x == b );
+        return x == a ? b : a;
+    }
+
+    bool operator<(Edge<Weight>& e){
+        return weight < e.wt();
+    }
+
+    bool operator<=(Edge<Weight>& e){
+        return weight <= e.wt();
+    }
+
+    bool operator>(Edge<Weight>& e){
+        return weight > e.wt();
+    }
+
+    bool operator>=(Edge<Weight>& e){
+        return weight >= e.wt();
+    }
+
+    bool operator==(Edge<Weight>& e){
+        return weight == e.wt();
+    }
+
+    friend ostream& operator<<(ostream &os, const Edge &e){
+        os<<e.a<<"-"<<e.b<<": "<<e.weight;
+        return os;
+    }
+};
